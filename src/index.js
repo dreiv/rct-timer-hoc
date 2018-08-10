9f16c3099eb9551e2bf0a92cbf9ca42c2d3abfc1
Andrei Voicu <dreivoicu@gmail.com>
2018-08-10 21:38:25 +0300

    good enough

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+// HoC to allow us to refresh a component on a ms interval
+const onInterval = refresh => WrappedComponent => {
+  return class WithInterval extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = { ticks: 0 };
+      this.interval = setInterval(this.tick.bind(this), refresh);
+    }
+
+    tick() {
+      this.setState(prevState => ({
+        ticks: ++prevState.ticks
+      }));
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+
+    render() {
+      return <WrappedComponent date={new Date()} />;
+    }
+  };
+};
+
 // Component to display current time
-const Time = ({ time }) => <h1>{time.toLocaleTimeString()}</h1>;
+const Time = ({ date }) => <h1>Current time: {date.toLocaleTimeString()}</h1>;
+
+// A timer by composing our <Time/> with HoC
+const Timer = onInterval(1000)(Time);
 
 function App() {
   return (
     <div className="App">
-      <Time time={new Date()} />
+      <Timer />
     </div>
   );
 }
